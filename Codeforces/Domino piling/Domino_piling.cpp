7f62ca927099e6d45ace6afe922c00e2a90105ba
Ganesh Pawar <58626718+ganpa3@users.noreply.github.com>
2020-08-09 12:56:52 +0530

    Create Domino_piling.cpp

---
+++ Codeforces/Domino piling/Domino_piling.cpp
@@ -0,0 +1,36 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/50/A
+* Author: ganpa
+*/
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int m, n;
+    cin >> m >> n;
+    cout << (m * n) / 2;
+}
