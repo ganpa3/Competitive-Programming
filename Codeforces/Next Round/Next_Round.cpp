d959e465ac4c11e1e488491c0a5f6f578b8cc657
Ganesh Pawar <58626718+ganpa3@users.noreply.github.com>
2020-08-09 12:43:44 +0530

    Create Next_Round.cpp

---
+++ Codeforces/Next Round/Next_Round.cpp
@@ -0,0 +1,47 @@
+/*
+ * Problem Statement: https://codeforces.com/problemset/problem/158/A
+ * Author: ganpa
+*/
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+typedef long long ll;
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(NULL);
+    int n, k, c = 0;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int a = arr[k - 1];
+    for (int j = 0; j < n; j++)
+    {
+        if (arr[j] >= a && arr[j] > 0)
+            c++;
+    }
+    cout << c;
+}
