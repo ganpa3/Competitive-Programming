9e0440e679f9983694cacb26beb9f7f8b68b9978
Ganesh Pawar <pawarg256@gmail.com>
2020-08-18 13:04:02 +0530

    Solved

---
+++ Codeforces/Worms.cpp
@@ -0,0 +1,38 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/474/B
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int n, m;
+    cin >> n;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++)
+    {
+        int x;
+        cin >> x;
+        a[i] = a[i - 1] + x;
+    }
+    cin >> m;
+    vector<int> b(m);
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    for (int i = 0; i < m; i++)
+    {
+        auto up = lower_bound(a.begin(), a.end(), b[i]);
+        if (up == a.end())
+            cout << n << "\n";
+        else
+            cout << up - a.begin() << "\n";
+    }
+}
