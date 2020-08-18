9f2c7a432a2128c6ca4d9c8ea5d5b6347a508eb3
Ganesh Pawar <pawarg256@gmail.com>
2020-08-18 22:00:45 +0530

    Solved

---
+++ Codeforces/Alternating_Subsequence.cpp
@@ -0,0 +1,43 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1343/C
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    auto check = [&](int x) {
+        if (x > 0)
+            return 1;
+        else
+            return -1;
+    };
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int& it : v)
+            cin >> it;
+        ll sum = 0;
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            int curr = v[i];
+            while (j < n && check(v[i]) == check(v[j])) {
+                curr = max(curr, v[j]);
+                j++;
+            }
+            sum += curr;
+            i = j - 1;
+        }
+        cout << sum << "\n";
+    }
+}
---
+++ Codeforces/Ternary_XOR.cpp
@@ -0,0 +1,42 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1328/C
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+typedef long long ll;
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int n;
+        cin >> n;
+        string x;
+        cin >> x;
+        string a = "1", b = "1";
+        for (int i = 1; i < n; ++i) {
+            if (x[i] == '0')
+                a += '0', b += '0';
+            else if (x[i] == '1') {
+                if (a < b)
+                    a += '1', b += '0';
+                else
+                    a += '0', b += '1';
+            } else if (x[i] == '2') {
+                if (a < b)
+                    a += '2', b += '0';
+                else
+                    a += '1', b += '1';
+            }
+        }
+        cout << a << "\n"
+             << b << "\n";
+    }
+}
