d7f88a48a609280a0084f7ca5492f5255a2d1223
Ganesh Pawar <pawarg256@gmail.com>
2020-08-23 12:39:22 +0530

    Solved

---
+++ Codeforces/Magical_Calendar.cpp
@@ -0,0 +1,30 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1371/B
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+#define endl '\n'
+typedef long long int ll;
+typedef long double ld;
+typedef unsigned long long int ull;
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        ll n, r;
+        cin >> n >> r;
+        ll x = n * (n + 1) / 2;
+        ll y = r * (r + 1) / 2;
+        if (n <= r)
+            cout << x - (n - 1) << endl;
+        else
+            cout << y << endl;
+    }
+}
