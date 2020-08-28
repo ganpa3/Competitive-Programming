e9e60a71754c8c52b5d67f0ebafb91382b046434
Ganesh Pawar <pawarg256@gmail.com>
2020-08-28 19:38:04 +0530

    Solved

---
+++ Codeforces/Perfect_Team.cpp
@@ -0,0 +1,26 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1221/C 
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int c, m, x;
+        cin >> c >> m >> x;
+        int ans = min({c, m, x});
+        c -= ans, m -= ans, x -= ans;
+        if (c == 0 || m == 0)
+            cout << ans << endl;
+        else
+            cout << ans + min({c, m, (c + m) / 3}) << endl;
+    }
+}
---
+++ Codeforces/Three_Parts_of_the_Array.cpp
@@ -0,0 +1,30 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1006/C 
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+#define endl '\n'
+#define int long long
+using namespace std;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int n, ans = 0;
+    cin >> n;
+    vector<int> v(n);
+    for (auto &it : v)
+        cin >> it;
+    int p1 = 0, p2 = n - 1, sum1 = 0, sum2 = 0;
+    while (p1 <= p2)
+    {
+        if (sum1 <= sum2)
+            sum1 += v[p1++];
+        else
+            sum2 += v[p2--];
+        if (sum1 == sum2)
+            ans = sum1;
+    }
+    cout << ans << endl;
+}
