c3db9627551f88763878f957074a45279434cb93
Ganesh Pawar <pawarg256@gmail.com>
2020-09-01 19:25:26 +0530

    Solved

---
+++ Codeforces/Creating_the_Contest.cpp
@@ -0,0 +1,29 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1029/B
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int &it : v)
+        cin >> it;
+
+    int ans = 1, temp = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (v[i] > v[i - 1] * 2)
+        {
+            ans = max(ans, temp);
+            temp = 0;
+        }
+        temp++;
+    }
+    cout << max(ans, temp) << endl;
+}
---
+++ Codeforces/Dominated_Subarray.cpp
@@ -0,0 +1,33 @@
+/*
+* Problem Statement: https://codeforces.com/problemset/problem/1257/C
+* Author: ganpa
+*/
+#include <bits/stdc++.h>
+
+#define sz(x) (int)(x).size()
+#define endl '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        vector<int> v(n), last(n + 1, -1);
+        for (int &it : v)
+            cin >> it;
+
+        int ans = n + 5;
+        for (int i = 0; i < n; i++)
+        {
+            if (last[v[i]] != -1)
+                ans = min(ans, i - last[v[i]] + 1);
+            last[v[i]] = i;
+        }
+        cout << (ans > n ? -1 : ans) << endl;
+    }
+}
