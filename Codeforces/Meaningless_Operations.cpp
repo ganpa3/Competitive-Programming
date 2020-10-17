f59304aa14f10dd8c98a1c59b8262d305353cc35
ganpa3 <pawarg256@gmail.com>
2020-10-17 15:48:20 +0530

    Solved

---
+++ Codeforces/Meaningless_Operations.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimize("Ofast")
+#define sz(x) (int)(x).size()
+#define all(v) v.begin(), v.end()
+#define endl '\n'
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+
+string to_string(string s) { return '"' + s + '"'; }
+
+string to_string(const char* s) { return to_string((string)s); }
+
+string to_string(bool b) { return (b ? "true" : "false"); }
+
+template <typename A, typename B>
+string to_string(pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A>
+string to_string(A v) {
+    bool first = true;
+    string res = "{";
+    for (const auto& x : v) {
+        if (!first) {
+            res += ", ";
+        }
+        first = false;
+        res += to_string(x);
+    }
+    res += "}";
+    return res;
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << to_string(H);
+    debug_out(T...);
+}
+
+#ifdef GANPA
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+bool isPowerOf2(int n) {
+    return !(n & (n - 1));
+}
+
+void solve() 
+{
+    int n;
+    cin >> n;
+    if (isPowerOf2(n+1)) {
+        int ans = 1;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                ans = n / i;
+                break;
+            }
+        }
+        cout << ans << endl;
+    } else {
+        n |= (n >> 1);
+        n |= (n >> 2);
+        n |= (n >> 4);
+        n |= (n >> 8);
+        n |= (n >> 16);
+        cout << n << endl;
+    }
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false), cin.tie(0);
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        solve();
+    }
+    return 0;
+}
